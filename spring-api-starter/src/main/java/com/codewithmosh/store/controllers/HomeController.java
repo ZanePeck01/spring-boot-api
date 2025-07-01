0755b5847fc8e64918fa0b4967f1537aa1368e87
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-07-01 12:15:37 -0400

    added thyemleaf and dynamic embedding of HTML

--- spring-api-starter/src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ spring-api-starter/src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -1,12 +1,22 @@
 package com.codewithmosh.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    /**
+     * This controller handles requests to the root URL and the /hello URL.
+     * It returns the index.html file for both requests.
+     */
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        // The model attribute "name" can be used in the Thymeleaf template.
+        model.addAttribute("name", "Zane");
+
         return "index.html";
     }
+
 }
