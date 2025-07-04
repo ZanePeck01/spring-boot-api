62db9d6f2c553bd45f8b2f1b3ca2b0864abffad8
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-07-04 12:40:55 -0400

    Built CRUD API for product

--- spring-api-starter/src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ spring-api-starter/src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -2,14 +2,23 @@ package com.codewithmosh.store.controllers;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 
 import lombok.AllArgsConstructor;
@@ -21,9 +30,10 @@ public class ProductController {
 
     private ProductRepository productRepository;
     private ProductMapper productMapper;
+    private CategoryRepository categoryRepository;
 
     @GetMapping
-    public List<ProductDto> getAllProducts(@RequestParam(name = "categoryId" ,required = false) Byte categoryId) {
+    public List<ProductDto> getAllProducts(@RequestParam(name = "categoryId", required = false) Byte categoryId) {
         List<Product> products;
         if (categoryId != null) {
             products = productRepository.findByCategoryId(categoryId);
@@ -31,6 +41,55 @@ public class ProductController {
             products = productRepository.findAllWithCategory();
         }
         return products.stream().map(productMapper::toDto).toList();
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto, UriComponentsBuilder uriBuilder) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(product.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productMapper.update(productDto, product);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        return ResponseEntity.ok(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.delete(product);
+        
+        return ResponseEntity.noContent().build();
     }
 
 }
--- spring-api-starter/target/generated-sources/annotations/com/codewithmosh/store/mappers/ProductMapperImpl.java
+++ spring-api-starter/target/generated-sources/annotations/com/codewithmosh/store/mappers/ProductMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-02T15:23:59-0400",
+    date = "2025-07-04T12:35:48-0400",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.50.v20250628-1110, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
@@ -29,6 +29,33 @@ public class ProductMapperImpl implements ProductMapper {
         productDto.setPrice( product.getPrice() );
 
         return productDto;
+    }
+
+    @Override
+    public Product toEntity(ProductDto productDto) {
+        if ( productDto == null ) {
+            return null;
+        }
+
+        Product.ProductBuilder product = Product.builder();
+
+        product.description( productDto.getDescription() );
+        product.id( productDto.getId() );
+        product.name( productDto.getName() );
+        product.price( productDto.getPrice() );
+
+        return product.build();
+    }
+
+    @Override
+    public void update(ProductDto productDto, Product product) {
+        if ( productDto == null ) {
+            return;
+        }
+
+        product.setDescription( productDto.getDescription() );
+        product.setName( productDto.getName() );
+        product.setPrice( productDto.getPrice() );
     }
 
     private Byte productCategoryId(Product product) {
